c7275955d8189ae9ef4adbf402ace0983849c68e
Riley <rileyguy665@hotmail.com>
2025-10-19 11:50:10 +0800

    module complete

--- PROJECT/script.js
+++ PROJECT/script.js
@@ -20,6 +20,25 @@ function removeStudent(name) {
 }
 
 function filterTopStudents(minGrade) {
-    return students.filter(student => student.grade >= mingrade);
-}
+    return students.filter(student => student.grade >= minGrade);
+};
+
+// function to map students in formatted list
+function formatStudentList() {
+    return students.map(student => `${student.name} - Grade: ${student.grade}`);
+};
+
+//start
+console.log('Students = ', students);
+
+//Add Students
+addStudent('Alice', 85);
+addStudent('Bob', 90);
+addStudent('Charles', 78);
+addStudent('Davey', 60);
+addStudent('Emma', 92);
+
+console.log('Students = ', students);
+console.log(filterTopStudents(80));
+
 
